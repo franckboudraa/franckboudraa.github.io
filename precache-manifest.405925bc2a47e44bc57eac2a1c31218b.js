40eab0eeebbdb447201b255e4dd7d91942410bae
Franck Boudraa <franckboudraa@gmail.com>
2019-04-15 19:49:25 +0200

    Updated portfolio content & dependencies

---
+++ precache-manifest.405925bc2a47e44bc57eac2a1c31218b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "19dd9cc18438230d4542e74e18df1908",
+    "url": "/static/media/wondyland.19dd9cc1.jpg"
+  },
+  {
+    "revision": "384fa2e418d378eb5095",
+    "url": "/static/css/main.d9528b24.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "42fed8cd167e9a3c4dc9fe164743cd8d",
+    "url": "/static/media/please.42fed8cd.jpg"
+  },
+  {
+    "revision": "f51eae8b8bbaeb3091f6",
+    "url": "/static/js/2.55b456c5.chunk.js"
+  },
+  {
+    "revision": "6bda18ddd093729fe31d71f4e57f0b21",
+    "url": "/static/media/pic.6bda18dd.jpg"
+  },
+  {
+    "revision": "dfd4bb68298b170ee9447fbf5c7c76ce",
+    "url": "/static/media/brooklyn3.dfd4bb68.jpg"
+  },
+  {
+    "revision": "384fa2e418d378eb5095",
+    "url": "/static/js/main.40c9e604.chunk.js"
+  },
+  {
+    "revision": "03bf514daafbccc0bfc450280ff88fa2",
+    "url": "/static/media/monsterwin.03bf514d.jpg"
+  },
+  {
+    "revision": "341eb02244966afca3cb6b054107f4ba",
+    "url": "/static/media/movegroove.341eb022.jpg"
+  },
+  {
+    "revision": "fc1f70bd857ceb1a7d5073e7372d1772",
+    "url": "/static/media/catmash.fc1f70bd.jpg"
+  },
+  {
+    "revision": "c2990253b0894733d6b551d92633b690",
+    "url": "/static/media/bbt.c2990253.jpg"
+  },
+  {
+    "revision": "f51eae8b8bbaeb3091f6",
+    "url": "/static/css/2.64123ff3.chunk.css"
+  },
+  {
+    "revision": "7dbefb5eba9f7600245040d0413d6b1c",
+    "url": "/index.html"
+  }
+];
